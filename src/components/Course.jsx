9d19a67508f4a58562f8c459c284d2f0d23c72ea
Junayed Jihad <junayedjihad48@gmail.com>
2024-06-22 12:09:51 +0600

    Lazy loading

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Course(props) {
   return (
     <div className='bg-slate-200 m-4 p-2 rounded-lg'>
-          <img src={props.image} alt="" />
+          <img src={props.image} alt="" loading='lazy' />
           <h2 className='uppercase font-semibold text-center text-xl mt-1'>{props.title}</h2>
     </div>
   )
